5db3208ed225344fbc89efd245286a05dd166b77
waliddx <wagzouly@google.com>
2024-09-13 14:43:32 +0100

    selection sort algorithm

---
+++ Arrays/01-Selection_sort.py
@@ -0,0 +1,16 @@
+"""
+The Selection Sort algorithm finds the lowest value in an array and moves it to the front of the array.
+"""
+
+my_array= [1, 64, 34, 1, 25, 5, 22, 11, 90, 12]
+
+n = len(my_array)
+
+for i in range(n):
+    min_idx = i
+    for j in range(i+1, n):
+        if my_array[j] < my_array[min_idx]:
+            min_idx = j
+    my_array[i], my_array[min_idx] = my_array[min_idx], my_array[i]
+
+print(my_array)
